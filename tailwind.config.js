7466cf22a35f4d35a49a43b7f4809313128f8188
Marcelo Arias <hello@marceloarias.com>
2022-10-11 21:56:14 -0500

    Create tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    backgroundColor: (theme) => theme("colors"),
+    borderColor: (theme) => theme("colors"),
     extend: {},
+    colors: {
+      reddarker: "#6A0012",
+      reddark: "#A00037",
+      redcandydark: "#D81B60",
+      redcandylight: "#FF5C8D",
+      redlight: "#FF90BD",
+      redlighter: "#FFC2EF",
+    }
   },
   plugins: [],
 }
