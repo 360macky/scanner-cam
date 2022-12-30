1a78c8992dc12d538d4dd67e3af1d23ec804f719
Marcelo Arias <hello@marceloarias.com>
2022-12-30 08:37:48 -0500

    Format unit tests with ESLint rules

--- tests/model.spec.ts
+++ tests/model.spec.ts
@@ -1,17 +1,17 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test'
 
 test('Home has a user flow to load the model', async ({ page }) => {
-  await page.goto('/');
+  await page.goto('/')
 
   // Check that there is a button element with the text "Start app"
-  expect(await page.textContent('button:has-text("Start app")')).toBeTruthy();
+  expect(await page.textContent('button:has-text("Start app")')).toBeTruthy()
 
   // Click the button element with the text "Start app"
-  await page.click('button:has-text("Start app")');
+  await page.click('button:has-text("Start app")')
 
   // Check that there is a p element with the text "Loading Computer Vision model..." OR "Cargando modelo de visión artificial..."
-  expect(await page.textContent('p:has-text("Loading Computer Vision model...")') || await page.textContent('p:has-text("Cargando modelo de visión artificial...")')).toBeTruthy();
+  expect(await page.textContent('p:has-text("Loading Computer Vision model...")') || await page.textContent('p:has-text("Cargando modelo de visión artificial...")')).toBeTruthy()
 
   // Wait until there is no div with the button with the text "Start app"
-  await page.waitForSelector('button:has-text("Start app")', { state: 'hidden' });
-});
+  await page.waitForSelector('button:has-text("Start app")', { state: 'hidden' })
+})
