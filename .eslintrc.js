33175417e5b38792ed0e62bdccaa48cc596662ef
Marcelo Arias <hello@marceloarias.com>
2022-12-29 23:04:28 -0500

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/prefer-optional-chain': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/triple-slash-reference': 'off',
+    '@typescript-eslint/prefer-nullish-coalescing': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    '@typescript-eslint/no-misused-promises': 'off',
+    'promise/param-names': 'off',
+    'no-unmodified-loop-condition': 'off',
+    'prefer-regex-literals': 'off'
+  },
+  ignorePatterns: ['node_modules', 'dist', 'build', 'coverage', 'public']
+}
