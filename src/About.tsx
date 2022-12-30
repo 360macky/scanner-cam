8a15be13d839f8ed1bcba72998c79d4aae654e56
Marcelo Arias <hello@marceloarias.com>
2022-12-30 08:32:01 -0500

    Format About.tsx

--- src/About.tsx
+++ src/About.tsx
@@ -1,26 +1,26 @@
-import classNames from "classnames";
-import React from "react";
-import { useTranslation } from "react-i18next";
+import classNames from 'classnames'
+import React from 'react'
+import { useTranslation } from 'react-i18next'
 
-type AboutProps = {
-  insideApp: boolean;
-};
+interface AboutProps {
+  insideApp: boolean
+}
 
-function About({ insideApp }: AboutProps) {
-  const { t } = useTranslation();
+function About ({ insideApp }: AboutProps) {
+  const { t } = useTranslation()
   return (
     <section
       className={classNames(
-        "min-h-screen bg-redcandylight dark:bg-reddarker p-4 py-16 lg:flex justify-center",
+        'min-h-screen bg-redcandylight dark:bg-reddarker p-4 py-16 lg:flex justify-center',
         { hidden: insideApp }
       )}
       id="about"
     >
       <div className="max-w-[1020px] bg-[white] dark:text-[white] dark:bg-reddark text-xl rounded-2xl p-8">
-        <b>Scanner Cam</b> {t("about.description")}
+        <b>Scanner Cam</b> {t('about.description')}
       </div>
     </section>
-  );
+  )
 }
 
-export default About;
+export default About
