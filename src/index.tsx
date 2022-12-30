8a34ba285da9335fbc87d62af12abd6bbd9e92f3
Marcelo Arias <hello@marceloarias.com>
2022-12-30 08:33:23 -0500

    Format index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
-import React, { Suspense } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import About from "./About";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-import reportWebVitals from "./reportWebVitals";
+import React, { Suspense } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import About from './About'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+import reportWebVitals from './reportWebVitals'
 
-import "./i18n";
+import './i18n'
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+  document.getElementById('root') as HTMLElement
+)
 
 root.render(
   <Suspense fallback={<div>Loading...</div>}>
@@ -22,14 +22,14 @@ root.render(
       </Routes>
     </BrowserRouter>
   </Suspense>
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.unregister()
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
