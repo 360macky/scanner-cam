883a23eb26dc67c41ad8a814cf8e2a85279baed7
Marcelo Arias <hello@marceloarias.com>
2022-12-29 22:27:33 -0500

    Create page.spec.ts

---
+++ tests/page.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+
+
+test('Home has page elements of ScannerCam', async ({ page }) => {
+  await page.goto('/');
+
+  // Check that there is a section element with the id "about"
+  expect(await page.textContent('section#about')).toBeTruthy();
+
+});
