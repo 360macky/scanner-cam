87639ce289c72e9182fb0fc6355f2b2c41bf56c5
Marcelo Arias <hello@marceloarias.com>
2022-12-29 23:29:51 -0500

    Update Tailwind configuration file with ESLint rules

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './src/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
-    backgroundColor: (theme) => theme("colors"),
-    borderColor: (theme) => theme("colors"),
+    backgroundColor: (theme) => theme('colors'),
+    borderColor: (theme) => theme('colors'),
     extend: {},
     colors: {
-      reddarker: "#6A0012",
-      reddark: "#A00037",
-      redcandydark: "#D81B60",
-      redcandylight: "#FF5C8D",
-      redlight: "#FF90BD",
-      redlighter: "#FFC2EF",
+      reddarker: '#6A0012',
+      reddark: '#A00037',
+      redcandydark: '#D81B60',
+      redcandylight: '#FF5C8D',
+      redlight: '#FF90BD',
+      redlighter: '#FFC2EF'
     }
   },
-  plugins: [],
+  plugins: []
 }
