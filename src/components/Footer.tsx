17254931f7e858fed26ee980d67a7b3232b2e8e6
Marcelo Arias <hello@marceloarias.com>
2023-02-15 18:26:14 -0500

    Add dark mode support to Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,7 +12,7 @@ interface FooterProps {
 
 function Footer({ t }: FooterProps) {
   return (
-    <footer className="p-4 bg-white shadow md:flex-col md:items-center md:justify-between md:p-6 md:pb-12 dark:bg-reddark">
+    <footer className="p-4 bg-white shadow md:flex-col md:items-center md:justify-between md:p-6 md:pb-12 dark:bg-redblackdark">
       <div className="flex justify-between w-full">
         <span className="text-sm text-gray-500 sm:text-center dark:text-white">
           © 2024{' '}
@@ -30,9 +30,14 @@ function Footer({ t }: FooterProps) {
         </ul>
       </div>
       <div className="flex gap-x-2 py-4">
-        <Image src={TensorFlowImage} height={25} alt="" />
-        <Image src={NextImage} height={25} alt="" />
-        <Image src={ReactImage} height={25} alt="" />
+        <Image
+          src={TensorFlowImage}
+          height={25}
+          alt=""
+          className="dark:invert"
+        />
+        <Image src={NextImage} height={25} alt="" className="dark:invert" />
+        <Image src={ReactImage} height={25} alt="" className="dark:invert" />
       </div>
     </footer>
   )
