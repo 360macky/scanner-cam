3c10301c4d7963a90c3dae06ae8d02dd56c86136
Marcelo Arias <hello@marceloarias.com>
2023-02-16 09:10:42 -0500

    Add googleusercontent.com to allowed domains of Next Image component

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@ const { i18n } = require('./next-i18next.config')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n
+  i18n,
+  images: {
+    domains: ['googleusercontent.com']
+  }
 }
 
 module.exports = nextConfig
