66a74fb61c39916384859dbd75c30fc538cbe3c5
Marcelo Arias <hello@marceloarias.com>
2023-02-16 15:09:52 -0500

    Opt-out design of Start button

--- src/components/StartButton.tsx
+++ src/components/StartButton.tsx
@@ -14,6 +14,8 @@ const StartButton = ({ onClick, title }: ButtonProps) => {
   const modelStatus: BROWSER_MODEL_STATUS = useSelector(
     (state: RootState) => state.app.modelStatus
   )
+  // Opt-out new classname syntax
+  // bg-gradient-to-br from-redlightdimmed to-reddark text-white hover:brightness-110 active:ring ring-redlight/60 w-auto self-center rounded-lg px-4 py-2 cursor-pointer text-[1.4rem] lg:text-[1.2rem] font-semibold
   return (
     <motion.button
       initial={{ opacity: 0, scale: 1.2 }}
@@ -21,7 +23,7 @@ const StartButton = ({ onClick, title }: ButtonProps) => {
       viewport={{ once: true }}
       transition={{ delay: 0.2, ease: 'easeInOut' }}
       className={classNames(
-        'bg-gradient-to-br from-redlightdimmed to-reddark text-white hover:brightness-110 active:ring ring-redlight/60 w-auto self-center rounded-lg px-4 py-2 cursor-pointer text-[1.4rem] lg:text-[1.2rem] font-semibold',
+        'bg-redlighter text-redblack hover:brightness-110 active:ring ring-redlight/60 w-auto self-center rounded-lg px-4 py-2 cursor-pointer text-[1.4rem] lg:text-[1.2rem] font-semibold',
         {
           hidden: modelStatus === 'READY'
         }
